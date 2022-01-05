b49ea85a67850d8e4b470e87825a3a3770ce74c5
dhanikprakash <dhanikprakash@gmail.com>
2022-01-05 20:19:36 +0000

    react jest enzyme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from "react";
+import { Button } from 'react-bootstrap'
+
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h2>Counter App</h2>
+     
       </header>
+      <div>
+      <Button id="increment-button" onClick={()=> setCounter(counter+1)}>Increment</Button>&nbsp;&nbsp;
+      <Button id="decrement-button" onClick={()=> setCounter(counter-1)}>Decrement</Button>
+      </div>
+      <br/>
+      <div id="counter">
+       <b>{counter}</b>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,42 @@
-import { render, screen } from '@testing-library/react';
+
+import { mount } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+let wrapper;
+
+beforeEach(()=>{
+  wrapper = mount(<App />)
+})
+
+afterEach(()=>{
+  wrapper.unmount();
+})
+
+describe("Counter Testing", () =>{
+  test('renders learn react link', () => {
+    
+    expect(wrapper.find('h2').text()).toBe('Counter App');
+  });
+
+  test('render increment', ()=>{
+    expect(wrapper.find('#increment-button').at(0).text()).toBe('Increment');
+  })
+
+  test('render descrement', ()=>{
+    expect(wrapper.find('#decrement-button').at(0).text()).toBe('Decrement');
+  })
+
+  test('Initial state of counter', ()=> {
+     expect(wrapper.find('#counter').text()).toBe('0');
+  })
+
+  test('render click event of increment and decrement button', ()=>{
+     wrapper.find('#increment-button').at(0).simulate('click');
+     expect(wrapper.find('#counter').text()).toBe('1');
+
+     wrapper.find('#decrement-button').at(0).simulate('click');
+     expect(wrapper.find('#counter').text()).toBe('0');
+  })
+  
+})
